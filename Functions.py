533ae871a9c8bf7f3178248aa8a489924d06212b
Bipin Neupane <bipinneupane08@gmail.com>
2024-01-26 18:08:25 +0000

    About FUnctions

---
+++ Functions.py
@@ -0,0 +1,12 @@
+"""
+In this repo, its basically the structure of the function and what it does:
+"""
+# function with no return type 
+def request(name):
+    ''' here def is the keyword used to define the function and (request) is the name of the function and (name) is the argument
+    that is passed to the function and use it for carrying out operations in the function'''
+    print("Hello ",name)
+
+request("Bipin")
+
+
