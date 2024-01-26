ca5fb8ae764d7c3790a669863c3b817cb5d58755
Bipin Neupane <bipinneupane08@gmail.com>
2024-01-26 18:12:40 +0000

    function with return value

--- Functions.py
+++ Functions.py
@@ -1,12 +1,19 @@
+import random
 """
 In this repo, its basically the structure of the function and what it does:
 """
 # function with no return type 
-def request(name):
-    ''' here def is the keyword used to define the function and (request) is the name of the function and (name) is the argument
-    that is passed to the function and use it for carrying out operations in the function'''
-    print("Hello ",name)
+# def request(name):
+#     ''' here def is the keyword used to define the function and (request) is the name of the function and (name) is the argument
+#     that is passed to the function and use it for carrying out operations in the function'''
+#     print("Hello ",name)
 
-request("Bipin")
+# request("Bipin")
 
+#function with return type 
+def request():
+    return random.randint(1,10)
+
+y=request()
+print("The random integer is ",str(y))
 
